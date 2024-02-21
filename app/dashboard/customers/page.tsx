3d439723823898a8e8a8a68de07f51f8fc7fb25a
Zach Stout <stout.zachary@gmail.com>
2024-02-21 09:33:29 -0700

    added in customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,18 @@
-export default function CustomerPage() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+
+export default async function CustomerPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: number;
+  };
+}) {
+  const customers = await fetchFilteredCustomers(searchParams?.query || '');
+  return (
+    <div>
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
