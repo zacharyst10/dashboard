ab3fd3c5289688281c4e65697caf94de974dbdbf
Zach Stout <stout.zachary@gmail.com>
2024-03-23 20:30:26 -0600

    fixed login form stuff

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          Please log in to continue to the dashboard.
         </h1>
         <div className="w-full">
           <div>
